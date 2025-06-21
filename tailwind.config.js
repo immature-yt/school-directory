f517667cfe3c75e1de0b5c3f6dd250cd31eefd54
Harjot Singh <harjotbhatia12@outlook.com>
2025-06-21 12:17:55 +0530

    Added global font and theme in layout.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif']
+      },
       animation: {
-        fadeIn: 'fadeIn 1.2s ease-in-out'
+        fadeIn: 'fadeIn 0.8s ease-out forwards',
+        pulseSlow: 'pulse 2.5s cubic-bezier(0.4, 0, 0.6, 1) infinite'
       },
       keyframes: {
         fadeIn: {
-          '0%': { opacity: 0 },
-          '100%': { opacity: 1 }
+          from: { opacity: 0, transform: 'translateY(20px)' },
+          to: { opacity: 1, transform: 'translateY(0)' }
         }
       }
     }
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('tailwindcss-animate')
-  ]
+  plugins: []
 }
