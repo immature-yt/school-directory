8e82f5a958a3d85570e277d61cc529f477736bef
Harjot Singh <harjotbhatia12@outlook.com>
2025-06-28 18:51:34 +0530

    Enable Vercel analytics

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation'
 import { SpeedInsights } from "@vercel/speed-insights/next"
-
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Home() {
   redirect('/add-school')
